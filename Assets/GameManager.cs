cca11740c8c5bf8d5baa70af0b52efdaff2365a5
peter.shen <peter.shen@kakaocorp.com>
2017-06-11 17:03:32 +0900

    Login -> GameRoom 개발 중

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,25 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
 	public ApiManager apiManager;
+	public CanvasManager canvasManager;
 
 	// Use this for initialization
 	void Start () {
+		// 현재 로그인은 Guest만 지원
+		GameObject googleLoginBtn = GameObject.Find ("LoginCanvas/GoogleLoginBtn");
+		googleLoginBtn.GetComponent<Button> ().interactable = false;
+		googleLoginBtn = GameObject.Find ("LoginCanvas/FacebookLoginBtn");
+		googleLoginBtn.GetComponent<Button> ().interactable = false;
+
+		canvasManager.Show (CanvasManager.CANVAS_TYPES.CANVAS_TYPE_LOGIN);
+		// App Initialzing data 획득
 		if (apiManager != null) {
 			StartCoroutine (apiManager.GetText ((returnValue) => {
 				Debug.Log (returnValue);
 			}));
 		} else {
+			// ErrorCode에 의한 팝업
 			Debug.LogError ("apiManager is NULL.");
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	public void Login(LoginManager.LOGIN_TYPES loginType) {
+		// Show game room canvas
+		canvasManager.Show (CanvasManager.CANVAS_TYPES.CANVAS_TYPE_GAME_ROOM);
+
+		// Disable event left/right buttons
+		GameObject arrowButton = GameObject.Find ("GameRoomCanvas/EventCanvas/LeftButton");
+		arrowButton.GetComponent<Button> ().interactable = false;
+		arrowButton = GameObject.Find ("GameRoomCanvas/EventCanvas/RightButton");
+		arrowButton.GetComponent<Button> ().interactable = false;
+
+		// Disable game mode left/right buttons
+		arrowButton = GameObject.Find ("GameRoomCanvas/ModeCanvas/LeftButton");
+		arrowButton.GetComponent<Button> ().interactable = false;
+		arrowButton = GameObject.Find ("GameRoomCanvas/ModeCanvas/RightButton");
+		arrowButton.GetComponent<Button> ().interactable = false;
+	}
+
 }
-;
--- Assets/Utils/ApiManager.cs
+++ Assets/Utils/ApiManager.cs
@@ -5,18 +5,34 @@ using UnityEngine.Networking;
 
 public class ApiManager : MonoBehaviour {
 
+	// App Initializing data
 	public string META_DATA_URL = "http://gachadoll.cafe24.com/test/api";
 
-	public HttpClient httpClient;
+	// Login
+	public string LOGIN_URL		= "http://gachadoll.cafe24.com/login";
 
-	public string GetMetaData() {
-		WWW www = httpClient.GET (META_DATA_URL);
-		if (www.error != null) {
-			return www.error;
-		} else {
-			return www.text;
-		}
-	}
+	// Game Room
+	public string GAME_ROOM_URL	= "http://gachadoll.cafe24.com/gameroom";
+
+	// Store
+	public string STORE_LIST_URL	= "http://gachadoll.cafe24.com/store/list";
+
+	// MyInfo
+	public string MYINFO_URL		= "http://gachadoll.cafe24.com/myinfo";
+	public string MYINFO_SET_URL	= "http://gachadoll.cafe24.com/myinfo/set";
+
+	// Postbox
+	public string POSTBOX_LIST_URL	= "http://gachadoll.cafe24.com/postbox/list";
+	public string POSTBOX_POST_URL	= "http://gachadoll.cafe24.com/postbox/post";
+
+	// Ranking
+	public string RANKING_LIST_URL	= "http://gachadoll.cafe24.com/ranking/list";
+
+	// Setting
+	public string SETTING_URL		= "http://gachadoll.cafe24.com/gameroom";
+	public string SETTING_SET_URL	= "http://gachadoll.cafe24.com/gameroom/set";
+
+	public HttpClient httpClient;
 
 	public IEnumerator GetText(System.Action<string> callback)
 	{
